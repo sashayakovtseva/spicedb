672765468dea23cd6ea04be710fdf4bb5845e122
sashayakovtseva <sashayakovtseva@gmail.com>
2024-03-19 11:50:49 +0300

    Rework watch and commit checkpoint every 5min
    Signed-off-by: sashayakovtseva <sashayakovtseva@gmail.com>

--- internal/datastore/ydb/watch.go
+++ internal/datastore/ydb/watch.go
@@ -54,8 +54,9 @@ func (y *ydbDatastore) Watch(
 }
 
 type cdcEvent struct {
-	Key      []cdcKeyElement
-	NewImage json.RawMessage
+	Topic    string          `json:"-"`
+	Key      []cdcKeyElement `json:"key"`
+	NewImage json.RawMessage `json:"newImage"`
 }
 
 type cdcKeyElement struct {
@@ -108,13 +109,6 @@ func (y *ydbDatastore) watch(
 	defer close(updates)
 	defer close(errs)
 
-	afterTimestampRevision, ok := afterRevision.(revisions.TimestampRevision)
-	if !ok {
-		errs <- fmt.Errorf("expected timestamp revision, got  %T", afterRevision)
-		return
-	}
-	readFromTime := afterTimestampRevision.Time()
-
 	tableToTopicName := map[string]string{
 		tableRelationTuple:   watchTopicName(y.config.tablePathPrefix, tableRelationTuple),
 		tableCaveat:          watchTopicName(y.config.tablePathPrefix, tableCaveat),
@@ -122,17 +116,12 @@ func (y *ydbDatastore) watch(
 	}
 	topicToTableName := lo.Invert(tableToTopicName)
 
-	topics := make([]string, 0, 3)
-	if opts.Content&datastore.WatchRelationships == datastore.WatchRelationships {
-		topics = append(topics, tableToTopicName[tableRelationTuple])
-	}
-	if opts.Content&datastore.WatchSchema == datastore.WatchSchema {
-		topics = append(topics, tableToTopicName[tableNamespaceConfig])
-		topics = append(topics, tableToTopicName[tableCaveat])
-	}
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	if len(topics) == 0 {
-		errs <- fmt.Errorf("at least relationships or schema must be specified")
+	cdcEvents, cdcErrors, err := y.cdcEvents(ctx, afterRevision, tableToTopicName, opts)
+	if err != nil {
+		errs <- fmt.Errorf("failed to read cdc events: %w", err)
 		return
 	}
 
@@ -164,9 +153,243 @@ func (y *ydbDatastore) watch(
 		case updates <- change:
 			return true
 		case <-timer.C:
-			errs <- datastore.NewWatchDisconnectedErr()
+			sendError(datastore.NewWatchDisconnectedErr())
 			return false
 		}
+	}
+
+	// WARNING! This is for test purpose only and only for namespace-aware schema watch!
+	// todo Fix this when 'resolved'-like messages are supported.
+	hackTicker := time.NewTicker(time.Minute * 5)
+	defer hackTicker.Stop()
+	const expectedChangesCount = 4
+
+	var (
+		tracked = common.NewChanges(revisions.TimestampIDKeyFunc, opts.Content)
+
+		changesCount    int
+		changesRevision revisions.TimestampRevision
+	)
+
+	for {
+		select {
+		case <-ctx.Done():
+			sendError(ctx.Err())
+			return
+		case err := <-cdcErrors:
+			sendError(fmt.Errorf("failed to read cdc events: %w", err))
+			return
+		case event, ok := <-cdcEvents:
+			if !ok {
+				continue
+			}
+
+			switch topicToTableName[event.Topic] {
+			case tableRelationTuple:
+				if len(event.Key) != 7 {
+					err := spiceerrors.MustBugf("unexpected PK size. want 7, got %d (%v)", len(event.Key), event.Key)
+					sendError(err)
+					return
+				}
+
+				createdAtUnixNano := event.Key[6].Int64
+				createRev := revisions.NewForTimestamp(createdAtUnixNano)
+
+				tuple := &core.RelationTuple{
+					ResourceAndRelation: &core.ObjectAndRelation{
+						Namespace: event.Key[0].String,
+						ObjectId:  event.Key[1].String,
+						Relation:  event.Key[2].String,
+					},
+					Subject: &core.ObjectAndRelation{
+						Namespace: event.Key[3].String,
+						ObjectId:  event.Key[4].String,
+						Relation:  event.Key[5].String,
+					},
+				}
+
+				if event.NewImage == nil {
+					break
+				}
+
+				var changes relationTupleImage
+				if err := json.Unmarshal(event.NewImage, &changes); err != nil {
+					sendError(fmt.Errorf("failed to unmarshal relation tuple new image: %w", err))
+					return
+				}
+
+				var structuredCtx map[string]any
+				if changes.CaveatContext != nil {
+					if err := json.Unmarshal(*changes.CaveatContext, &structuredCtx); err != nil {
+						sendError(fmt.Errorf("failed to unmarshal caveat context new image: %w", err))
+						return
+					}
+				}
+
+				ctxCaveat, err := common.ContextualizedCaveatFrom(lo.FromPtr(changes.CaveatName), structuredCtx)
+				if err != nil {
+					sendError(err)
+					return
+				}
+				tuple.Caveat = ctxCaveat
+
+				if changes.DeletedAtUnixNano != nil {
+					deleteRev := revisions.NewForTimestamp(*changes.DeletedAtUnixNano)
+					err := tracked.AddRelationshipChange(ctx, deleteRev, tuple, core.RelationTupleUpdate_DELETE)
+					if err != nil {
+						sendError(err)
+						return
+					}
+					break
+				}
+
+				if err := tracked.AddRelationshipChange(
+					ctx,
+					createRev,
+					tuple,
+					core.RelationTupleUpdate_TOUCH,
+				); err != nil {
+					sendError(err)
+					return
+				}
+
+			case tableNamespaceConfig:
+				if len(event.Key) != 2 {
+					err := spiceerrors.MustBugf("unexpected PK size. want 2, got %d (%v)", len(event.Key), event.Key)
+					sendError(err)
+					return
+				}
+
+				namespaceName := event.Key[0].String
+				createRev := revisions.NewForTimestamp(event.Key[1].Int64)
+
+				if event.NewImage == nil {
+					break
+				}
+
+				var changes namespaceConfigImage
+				if err := json.Unmarshal(event.NewImage, &changes); err != nil {
+					sendError(fmt.Errorf("failed to unmarshal namespace new image: %w", err))
+					return
+				}
+				changesCount++
+				changesRevision = createRev
+
+				if changes.DeletedAtUnixNano != nil {
+					deleteRev := revisions.NewForTimestamp(*changes.DeletedAtUnixNano)
+					tracked.AddDeletedNamespace(ctx, deleteRev, namespaceName)
+					break
+				}
+
+				var loaded core.NamespaceDefinition
+				if err := loaded.UnmarshalVT(changes.SerializedConfig); err != nil {
+					sendError(fmt.Errorf("failed to unmarshal namespace config: %w", err))
+					return
+				}
+				tracked.AddChangedDefinition(ctx, createRev, &loaded)
+
+			case tableCaveat:
+				if len(event.Key) != 2 {
+					err := spiceerrors.MustBugf("unexpected PK size. want 2, got %d (%v)", len(event.Key), event.Key)
+					sendError(err)
+					return
+				}
+
+				caveatName := event.Key[0].String
+				createRev := revisions.NewForTimestamp(event.Key[1].Int64)
+
+				if event.NewImage == nil {
+					break
+				}
+
+				var changes caveatImage
+				if err := json.Unmarshal(event.NewImage, &changes); err != nil {
+					sendError(fmt.Errorf("failed to unmarshal caveat new image: %w", err))
+					return
+				}
+
+				if changes.DeletedAtUnixNano != nil {
+					deleteRev := revisions.NewForTimestamp(*changes.DeletedAtUnixNano)
+					tracked.AddDeletedNamespace(ctx, deleteRev, caveatName)
+					break
+				}
+
+				var loaded core.CaveatDefinition
+				if err := loaded.UnmarshalVT(changes.Definition); err != nil {
+					sendError(fmt.Errorf("failed to unmarshal caveat config: %w", err))
+					return
+				}
+				tracked.AddChangedDefinition(ctx, createRev, &loaded)
+			}
+
+			if changesCount == expectedChangesCount {
+				changes := tracked.AsRevisionChanges(revisions.TimestampIDKeyLessThanFunc)
+				for i := range changes {
+					if !sendChange(&changes[i]) {
+						return
+					}
+				}
+
+				// WARNING! This is for test purpose only and only for namespace-aware schema watch!
+				// todo Fix this when 'resolved'-like messages are supported.
+				futureCheckpoint := revisions.NewForTime(changesRevision.Time().Add(time.Minute * 30))
+				if opts.Content&datastore.WatchCheckpoints == datastore.WatchCheckpoints {
+					if !sendChange(&datastore.RevisionChanges{
+						Revision:     futureCheckpoint,
+						IsCheckpoint: true,
+					}) {
+						return
+					}
+				}
+
+				changesCount = 0
+				changesRevision = 0
+				tracked = common.NewChanges(revisions.TimestampIDKeyFunc, opts.Content)
+			}
+		case now := <-hackTicker.C:
+			if opts.Content&datastore.WatchCheckpoints == datastore.WatchCheckpoints {
+				if !sendChange(&datastore.RevisionChanges{
+					Revision:     revisions.NewForTime(now.Add(time.Minute * 30)),
+					IsCheckpoint: true,
+				}) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func watchTopicName(tablePathPrefix string, tableName string) string {
+	topicName := tableName + "/" + changefeedSpicedbWatch
+	if tablePathPrefix == "" {
+		return topicName
+	}
+	return tablePathPrefix + "/" + topicName
+}
+
+func (y *ydbDatastore) cdcEvents(
+	ctx context.Context,
+	afterRevision datastore.Revision,
+	tableToTopicName map[string]string,
+	opts datastore.WatchOptions,
+) (<-chan cdcEvent, <-chan error, error) {
+	afterTimestampRevision, ok := afterRevision.(revisions.TimestampRevision)
+	if !ok {
+		return nil, nil, fmt.Errorf("expected timestamp revision, got  %T", afterRevision)
+	}
+	readFromTime := afterTimestampRevision.Time()
+
+	topics := make([]string, 0, 3)
+	if opts.Content&datastore.WatchRelationships == datastore.WatchRelationships {
+		topics = append(topics, tableToTopicName[tableRelationTuple])
+	}
+	if opts.Content&datastore.WatchSchema == datastore.WatchSchema {
+		topics = append(topics, tableToTopicName[tableNamespaceConfig])
+		topics = append(topics, tableToTopicName[tableCaveat])
+	}
+
+	if len(topics) == 0 {
+		return nil, nil, fmt.Errorf("at least relationships or schema must be specified")
 	}
 
 	consumerUUID := uuid.New().String()
@@ -178,12 +401,7 @@ func (y *ydbDatastore) watch(
 
 	var selectors topicoptions.ReadSelectors
 	for _, topic := range topics {
-		selectors = append(selectors,
-			topicoptions.ReadSelector{
-				Path:     topic,
-				ReadFrom: readFromTime,
-			},
-		)
+		selectors = append(selectors, topicoptions.ReadSelector{Path: topic, ReadFrom: readFromTime})
 
 		if err := y.driver.Topic().Alter(ctx, topic,
 			topicoptions.AlterWithAddConsumers(topictypes.Consumer{
@@ -192,18 +410,8 @@ func (y *ydbDatastore) watch(
 				ReadFrom:        readFromTime,
 			}),
 		); err != nil {
-			sendError(fmt.Errorf("failed to create consumer: %w", err))
-			return
+			return nil, nil, fmt.Errorf("failed to create consumer: %w", err)
 		}
-		defer func(topic string) {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-			defer cancel()
-
-			err := y.driver.Topic().Alter(ctx, topic, topicoptions.AlterWithDropConsumers(consumerUUID))
-			if err != nil {
-				log.Warn().Str("topic", topic).Str("consumer", consumerUUID).Err(err).Msg("failed to remove consumer")
-			}
-		}(topic)
 	}
 
 	reader, err := y.driver.Topic().StartReader(
@@ -212,229 +420,79 @@ func (y *ydbDatastore) watch(
 		topicoptions.WithReaderCommitMode(topicoptions.CommitModeSync),
 	)
 	if err != nil {
-		sendError(fmt.Errorf("failed to create reader: %w", err))
-		return
+		return nil, nil, fmt.Errorf("failed to create reader: %w", err)
 	}
-	defer func() { _ = reader.Close(ctx) }()
 
 	if err := reader.WaitInit(ctx); err != nil {
-		sendError(fmt.Errorf("failed to await reader initialization: %w", err))
-		return
+		return nil, nil, fmt.Errorf("failed to await reader initialization: %w", err)
 	}
 
-	// WARNING! This is for test purpose only and only for namespace-aware schema watch!
-	// todo Fix this when 'resolved'-like messages are supported.
-	const expectedChangesCount = 4
+	messages := make(chan cdcEvent, 1)
+	errCh := make(chan error, 3)
+	go func() {
+		defer close(messages)
+		defer close(errCh)
 
-	var (
-		rErr    error
-		msg     *topicreader.Message
-		event   cdcEvent
-		tracked = common.NewChanges(revisions.TimestampIDKeyFunc, opts.Content)
-
-		changesCount    int
-		changesRevision revisions.TimestampRevision
-	)
-
-	for msg, rErr = reader.ReadMessage(ctx); rErr == nil; msg, rErr = reader.ReadMessage(ctx) {
-		if err := topicsugar.JSONUnmarshal(msg, &event); err != nil {
-			sendError(fmt.Errorf("failed to unmarshal cdc event: %w", err))
-			return
-		}
-
-		log.Trace().
-			Str("topic", msg.Topic()).
-			Int64("partition", msg.PartitionID()).
-			Str("message_group_id", msg.MessageGroupID).
-			Str("producer_id", msg.ProducerID).
-			Int64("seq_no", msg.SeqNo).
-			Int64("offset", msg.Offset).
-			Time("created_at", msg.CreatedAt).
-			Time("written_at", msg.WrittenAt).
-			Any("metadata", msg.Metadata).
-			Any("event", event).
-			Msg("got new YDB CDC event")
-
-		switch topicToTableName[msg.Topic()] {
-		case tableRelationTuple:
-			if len(event.Key) != 7 {
-				sendError(spiceerrors.MustBugf("unexpected PK size. want 7, got %d (%v)", len(event.Key), event.Key))
-				return
-			}
-
-			createdAtUnixNano := event.Key[6].Int64
-			createRev := revisions.NewForTimestamp(createdAtUnixNano)
-
-			tuple := &core.RelationTuple{
-				ResourceAndRelation: &core.ObjectAndRelation{
-					Namespace: event.Key[0].String,
-					ObjectId:  event.Key[1].String,
-					Relation:  event.Key[2].String,
-				},
-				Subject: &core.ObjectAndRelation{
-					Namespace: event.Key[3].String,
-					ObjectId:  event.Key[4].String,
-					Relation:  event.Key[5].String,
-				},
-			}
-
-			if event.NewImage == nil {
-				break
-			}
-
-			var changes relationTupleImage
-			if err := json.Unmarshal(event.NewImage, &changes); err != nil {
-				sendError(fmt.Errorf("failed to unmarshal relation tuple new image: %w", err))
-				return
-			}
-
-			var structuredCtx map[string]any
-			if changes.CaveatContext != nil {
-				if err := json.Unmarshal(*changes.CaveatContext, &structuredCtx); err != nil {
-					sendError(fmt.Errorf("failed to unmarshal caveat context new image: %w", err))
-					return
-				}
-			}
-
-			ctxCaveat, err := common.ContextualizedCaveatFrom(lo.FromPtr(changes.CaveatName), structuredCtx)
-			if err != nil {
-				sendError(err)
-				return
-			}
-			tuple.Caveat = ctxCaveat
-
-			if changes.DeletedAtUnixNano != nil {
-				deleteRev := revisions.NewForTimestamp(*changes.DeletedAtUnixNano)
-				err := tracked.AddRelationshipChange(ctx, deleteRev, tuple, core.RelationTupleUpdate_DELETE)
-				if err != nil {
-					sendError(err)
-					return
-				}
-				break
-			}
-
-			if err := tracked.AddRelationshipChange(ctx, createRev, tuple, core.RelationTupleUpdate_TOUCH); err != nil {
-				sendError(err)
-				return
-			}
-
-		case tableNamespaceConfig:
-			if len(event.Key) != 2 {
-				sendError(spiceerrors.MustBugf("unexpected PK size. want 2, got %d (%v)", len(event.Key), event.Key))
-				return
-			}
-
-			namespaceName := event.Key[0].String
-			createRev := revisions.NewForTimestamp(event.Key[1].Int64)
-
-			if event.NewImage == nil {
-				break
-			}
-
-			var changes namespaceConfigImage
-			if err := json.Unmarshal(event.NewImage, &changes); err != nil {
-				sendError(fmt.Errorf("failed to unmarshal namespace new image: %w", err))
-				return
-			}
-			changesCount++
-			changesRevision = createRev
-
-			if changes.DeletedAtUnixNano != nil {
-				deleteRev := revisions.NewForTimestamp(*changes.DeletedAtUnixNano)
-				tracked.AddDeletedNamespace(ctx, deleteRev, namespaceName)
-				break
-			}
-
-			var loaded core.NamespaceDefinition
-			if err := loaded.UnmarshalVT(changes.SerializedConfig); err != nil {
-				sendError(fmt.Errorf("failed to unmarshal namespace config: %w", err))
-				return
-			}
-			tracked.AddChangedDefinition(ctx, createRev, &loaded)
-
-		case tableCaveat:
-			if len(event.Key) != 2 {
-				sendError(spiceerrors.MustBugf("unexpected PK size. want 2, got %d (%v)", len(event.Key), event.Key))
-				return
-			}
-
-			caveatName := event.Key[0].String
-			createRev := revisions.NewForTimestamp(event.Key[1].Int64)
-
-			if event.NewImage == nil {
-				break
-			}
-
-			var changes caveatImage
-			if err := json.Unmarshal(event.NewImage, &changes); err != nil {
-				sendError(fmt.Errorf("failed to unmarshal caveat new image: %w", err))
-				return
-			}
-
-			if changes.DeletedAtUnixNano != nil {
-				deleteRev := revisions.NewForTimestamp(*changes.DeletedAtUnixNano)
-				tracked.AddDeletedNamespace(ctx, deleteRev, caveatName)
-				break
-			}
-
-			var loaded core.CaveatDefinition
-			if err := loaded.UnmarshalVT(changes.Definition); err != nil {
-				sendError(fmt.Errorf("failed to unmarshal caveat config: %w", err))
-				return
-			}
-			tracked.AddChangedDefinition(ctx, createRev, &loaded)
-		}
-
-		if err := reader.Commit(ctx, msg); err != nil {
-			sendError(fmt.Errorf("failed to commit offset: %w", err))
-			return
-		}
-
-		if changesCount == expectedChangesCount {
-			changes := tracked.AsRevisionChanges(revisions.TimestampIDKeyLessThanFunc)
-			for i := range changes {
-				if !sendChange(&changes[i]) {
-					return
-				}
-			}
-
-			// WARNING! This is for test purpose only and only for namespace-aware schema watch!
-			// todo Fix this when 'resolved'-like messages are supported.
-			futureCheckpoint := revisions.NewForTime(changesRevision.Time().Add(time.Hour * 24 * 30))
-			if opts.Content&datastore.WatchCheckpoints == datastore.WatchCheckpoints {
-				if !sendChange(&datastore.RevisionChanges{
-					Revision:     futureCheckpoint,
-					IsCheckpoint: true,
-				}) {
-					return
-				}
-			}
-
-			changesCount = 0
-			changesRevision = 0
-			tracked = common.NewChanges(revisions.TimestampIDKeyFunc, opts.Content)
-		}
-	}
-
-	if rErr != nil {
-		if errors.Is(ctx.Err(), context.Canceled) {
+		defer func() {
 			closeCtx, closeCancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer closeCancel()
 			if err := reader.Close(closeCtx); err != nil {
-				errs <- err
+				errCh <- fmt.Errorf("failed to close reader: %w", err)
+			}
+		}()
+
+		defer func() {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			for _, topic := range topics {
+				err := y.driver.Topic().Alter(
+					ctx,
+					topic,
+					topicoptions.AlterWithDropConsumers(consumerUUID),
+				)
+				if err != nil {
+					errCh <- fmt.Errorf("failed to remove consumer %q: %w", consumerUUID, err)
+				}
+			}
+		}()
+
+		var (
+			rErr error
+			msg  *topicreader.Message
+		)
+		for msg, rErr = reader.ReadMessage(ctx); rErr == nil; msg, rErr = reader.ReadMessage(ctx) {
+			var event cdcEvent
+			if err := topicsugar.JSONUnmarshal(msg, &event); err != nil {
+				errCh <- fmt.Errorf("failed to unmarshal cdc event: %w", err)
 				return
 			}
-			errs <- datastore.NewWatchCanceledErr()
-		} else {
-			errs <- rErr
-		}
-	}
-}
+			event.Topic = msg.Topic()
 
-func watchTopicName(tablePathPrefix string, tableName string) string {
-	topicName := tableName + "/" + changefeedSpicedbWatch
-	if tablePathPrefix == "" {
-		return topicName
-	}
-	return tablePathPrefix + "/" + topicName
+			log.Trace().
+				Str("topic", msg.Topic()).
+				Int64("partition", msg.PartitionID()).
+				Str("message_group_id", msg.MessageGroupID).
+				Str("producer_id", msg.ProducerID).
+				Int64("seq_no", msg.SeqNo).
+				Int64("offset", msg.Offset).
+				Time("created_at", msg.CreatedAt).
+				Time("written_at", msg.WrittenAt).
+				Any("metadata", msg.Metadata).
+				Any("event", event).
+				Msg("got new YDB CDC event")
+
+			messages <- event
+
+			if err := reader.Commit(ctx, msg); err != nil {
+				errCh <- fmt.Errorf("failed to commit offset: %w", err)
+				return
+			}
+		}
+
+		if rErr != nil && !errors.Is(ctx.Err(), context.Canceled) {
+			errCh <- fmt.Errorf("failed to read cdc events: %w", rErr)
+		}
+	}()
+
+	return messages, errCh, nil
 }
