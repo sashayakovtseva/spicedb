7c758db89dc7132a7a7f4dba0dcc959d2c739498
sashayakovtseva <sashayakovtseva@gmail.com>
2024-03-26 14:31:27 +0300

    Remove ydb otel tracer
    Signed-off-by: sashayakovtseva <sashayakovtseva@gmail.com>

--- internal/datastore/ydb/ydb.go
+++ internal/datastore/ydb/ydb.go
@@ -9,7 +9,6 @@ import (
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/prometheus/client_golang/prometheus"
-	ydbOtel "github.com/ydb-platform/ydb-go-sdk-otel"
 	ydbPrometheus "github.com/ydb-platform/ydb-go-sdk-prometheus"
 	ydbZerolog "github.com/ydb-platform/ydb-go-sdk-zerolog"
 	"github.com/ydb-platform/ydb-go-sdk/v3"
@@ -76,7 +75,6 @@ func newYDBDatastore(ctx context.Context, dsn string, opts ...Option) (*ydbDatas
 
 	ydbOpts := []ydb.Option{
 		ydbZerolog.WithTraces(&log.Logger, trace.DatabaseSQLEvents),
-		ydbOtel.WithTraces(),
 		ydb.WithSessionPoolSizeLimit(config.sessionCountLimit),
 		ydb.WithSessionPoolIdleThreshold(config.sessionIdleThreshold),
 		ydb.WithSessionPoolKeepAliveTimeout(config.sessionKeepaliveTimeout),
